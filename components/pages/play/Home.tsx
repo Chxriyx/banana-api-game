ae0570efbc5f4aa2089e36fba7f2a82eb4389082
Chxriyx <charithwijenayake.2000@gmail.com>
2024-11-16 13:08:56 +0530

    home page frontend implementation

---
+++ components/pages/play/Home.tsx
@@ -0,0 +1,24 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div
+      className="flex flex-col items-center min-h-screen bg-center justify-around"
+      style={{ backgroundImage: "url('/images/banana-game-bg-img.png')" }}
+    >
+      <h1 className="text-6xl font-bold bg-[#D9D9D9] bg-opacity-60 rounded-2xl p-6">
+        Banana Quiz
+      </h1>
+      <div className="flex justify-center animate-bounce">
+        <Link href="/play">
+          <Button className="bg-orange-500 px-24 py-12 rounded-full text-black font-bold transition-all duration-500">
+            <span className="text-5xl animate-bounce-text">Play</span>
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -52,6 +52,7 @@ const config = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
+        orangeCustom: "#FFA500",
       },
       borderRadius: {
         lg: "var(--radius)",
