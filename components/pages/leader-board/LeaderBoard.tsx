241529519fab85c1c2924827e95d9bcadb25655a
Chxriyx <charithwijenayake.2000@gmail.com>
2024-12-04 01:02:53 +0530

    leader board design change

--- components/pages/leader-board/LeaderBoard.tsx
+++ components/pages/leader-board/LeaderBoard.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
 
 export default function Home() {
-  const [scores, setScores] = useState([]);
+  const [scores, setScores] = useState({} as { [key: string]: any[] });
 
   useEffect(() => {
     const fetchScores = async () => {
@@ -16,15 +16,39 @@ export default function Home() {
       const { data, error } = await supabase
         .from("scores")
         .select("user_name, score, difficulty")
-        .order("score", { ascending: false })
-        .limit(5);
+        .order("score", { ascending: false });
 
       if (error) console.error("Error fetching scores:", error);
-      else setScores(data);
+      else {
+        const scoresByDifficulty = {} as { [key: string]: any[] };
+        data.forEach(score => {
+          if (!scoresByDifficulty[score.difficulty]) scoresByDifficulty[score.difficulty] = [];
+          scoresByDifficulty[score.difficulty].push(score);
+        });
+        setScores(scoresByDifficulty);
+      }
     };
 
     fetchScores();
   }, []);
+
+  const renderScores = (difficulty: string) => (
+    <div className="bg-[#FFA500] bg-opacity-80 rounded-2xl mt-10 p-6 m-4 w-3/5 flex flex-col">
+      <h2 className="text-4xl font-bold mb-4">{difficulty} Level</h2>
+      <div className="flex flex-col">
+        {scores[difficulty] ? (
+          scores[difficulty].slice(0, 5).map((score, index) => (
+            <div key={index} className="flex items-center justify-between text-2xl py-4 mx-6 border-b border-black">
+              <p className="font-bold text-3xl">{score.user_name}</p>
+              <p className="font-bold text-3xl">{score.score}</p>
+            </div>
+          ))
+        ) : (
+          <p className="text-3xl text-center">No data</p>
+        )}
+      </div>
+    </div>
+  );
 
   return (
     <div
@@ -42,16 +66,7 @@ export default function Home() {
       <h1 className="text-6xl font-bold bg-[#D9D9D9] bg-opacity-60 rounded-2xl p-6">
         Leader Board
       </h1>
-      <div className="bg-[#FFA500] bg-opacity-80 rounded-2xl mt-10 p-6 m-4 w-3/5">
-        <h2 className="text-4xl font-bold mb-4">Top five</h2>
-        {scores.map((score, index) => (
-          <div key={index} className="flex items-center justify-between text-2xl py-4 mx-6">
-            <p className="font-bold text-3xl">{score.user_name}</p>
-            <p className="font-bold text-3xl">{score.difficulty}</p>
-            <p className="font-bold text-3xl">{score.score}</p>
-          </div>
-        ))}
-      </div>
+      <div className="flex space-x-4 w-[90%]">{renderScores("Easy")}{renderScores("Medium")}{renderScores("Hard")}</div>
     </div>
   );
 }
